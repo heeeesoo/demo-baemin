3736675969dc58831eef907f547c40b18765f16f
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2024-05-03 15:40:28 +0900

    first commit

---
+++ src/main/java/com/example/demobaemin/DemoBaeminApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobaemin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBaeminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBaeminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demobaemin/DemoBaeminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demobaemin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBaeminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
